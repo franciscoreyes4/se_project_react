0f82dd9fe292414e56b62bfd2db7512974ce019f
Francisco Reyes <fjreyesq@gmail.com>
2024-09-23 01:07:43 -0700

    open and close modal

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -1,9 +1,52 @@
-
-import './App.css'
+import { useState } from "react";
+import './App.css';
+import Header from '../Header/Header';
+import Main from "../Main/Main";
+import WeatherCard from '../WeatherCard/WeatherCard';
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
-  return
-    <div>  </div>;
+  const [weatherData, setWeatherData] = useState({ type: ""});
+  const [activeModal, setActiveModal] = useState("")
+
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  }
+
+  return (
+    <div className="app">
+      <div className="app__content">
+        <Header  handleAddClick={handleAddClick}/>
+        <Main weatherData={weatherData}/>
+        <Footer />
+      </div>
+      <ModalWithForm title="New garment" buttonText="Add garment" activeModal={activeModal} handleCloseClick={closeActiveModal}>
+      <label htmlFor="name" className="modal__label" >
+                Name{" "} <input type="text" className="modal__input" id="name" placeholder="Name"/>
+            </label>
+            <label htmlFor="imageURL" className="modal__label">
+                Image{" "} <input type="url" className="modal__input" id="imageURL" placeholder="Image URL"/>
+            </label>
+            <fieldset className="modal__radio-buttons">
+            <legend className="moda__legend">Select the weather type:</legend>
+            <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            </label>
+            <label htmlFor="warm" className="modal__label modal__label_type_radio">
+            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            </label>
+            <label htmlFor="cold" className="modal__label modal__label_type_radio">
+            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            </label>
+            </fieldset>
+      </ModalWithForm>
+    </div>
+  );
 }
 
-export default App
+
+export default App;
---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header(handleAddClick) {
+    return (
+    <header className="header">
+        <img className="header__logo" src={logo}/>
+        <p className="header__date-and-location">DATE, LOCATION</p>
+        <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">+ Add clothes</button>
+        <div className="header__user">
+            <p className="header__username">Terrence Tegegne</p>
+            <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
+    </header>
+    );
+}
+
+export default Header;
